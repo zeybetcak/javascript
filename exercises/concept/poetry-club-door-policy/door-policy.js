1e249c919f6521766a899d6d89ed6d6210fec3af
zeybetcak <63880441+zeybetcak@users.noreply.github.com>
2022-02-12 15:14:52 -0800

    finished door policy exercise

--- exercises/concept/poetry-club-door-policy/door-policy.js
+++ exercises/concept/poetry-club-door-policy/door-policy.js
@@ -18,7 +18,6 @@
 // in idiomatic JavaScript, but some companies and style-guides do enforce them.
 //
 // Good luck with that door policy!
-
 /**
  * Respond with the correct character, given the line of the
  * poem, if this were said at the front door.
@@ -26,39 +25,43 @@
  * @param {string} line
  * @returns {string}
  */
-export function frontDoorResponse(line) {
-  throw new Error('Implement the frontDoorResponse function');
-}
-
-/**
- * Format the password for the front-door, given the response
- * letters.
- *
- * @param {string} word the letters you responded with before
- * @returns {string} the front door password
- */
-export function frontDoorPassword(word) {
-  throw new Error('Implement the frontDoorPassword function');
-}
-
-/**
- * Respond with the correct character, given the line of the
- * poem, if this were said at the back door.
- *
- * @param {string} line
- * @returns {string}
- */
-export function backDoorResponse(line) {
-  throw new Error('Implement the backDoorResponse function');
-}
-
-/**
- * Format the password for the back door, given the response
- * letters.
- *
- * @param {string} word the letters you responded with before
- * @returns {string} the back door password
- */
-export function backDoorPassword(word) {
-  throw new Error('Implement the backDoorPassword function');
-}
+ export function frontDoorResponse(line) {
+  return line.charAt(0)
+  }
+  /**
+   * Format the password for the front-door, given the response
+   * letters.
+   *
+   * @param {string} word the letters you responded with before
+   * @returns {string} the front door password
+   */
+  export function frontDoorPassword(word) {
+    let firstLetter = word[0].toUpperCase();
+    let lastFour = (word.slice(1)).toLowerCase(); 
+    return firstLetter 
+  + lastFour
+  }
+  /**
+   * Respond with the correct character, given the line of the
+   * poem, if this were said at the back door.
+   *
+   * @param {string} line
+   * @returns {string}
+   */
+  export function backDoorResponse(line) {
+    return line.charAt(line.trim().length-1)
+  }
+  /**
+   * Format the password for the back door, given the response
+   * letters.
+   *
+   * @param {string} word the letters you responded with before
+   * @returns {string} the back door password
+   */
+  export function backDoorPassword(word) {
+    let firstLetter = word[0].toUpperCase();
+    let lastFour = (word.slice(1)).toLowerCase(); 
+    return (firstLetter 
+  + lastFour) + ", please";
+  }
+  
