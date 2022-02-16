4905b719f35c0769bd3bb1228a89fcb555a733ab
zeybetcak <63880441+zeybetcak@users.noreply.github.com>
2022-02-16 08:12:44 -0800

    completed annalyns-infiltration, elyses-enchantments, and vehicle-purchase exercise

--- exercises/concept/annalyns-infiltration/annalyns-infiltration.js
+++ exercises/concept/annalyns-infiltration/annalyns-infiltration.js
@@ -26,50 +26,72 @@
  *
  * @return {boolean} Whether or not you can execute a fast attack.
  */
-export function canExecuteFastAttack(knightIsAwake) {
-  throw new Error('Implement the canExecuteFastAttack function');
-}
-
-/**
- * A useful spy captures information, which they can't do if everyone's asleep.
- *
- * @param {boolean} knightIsAwake
- * @param {boolean} archerIsAwake
- * @param {boolean} prisonerIsAwake
- *
- * @returns {boolean} Whether or not you can spy on someone.
- */
-export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
-  throw new Error('Implement the canSpy function');
-}
-
-/**
- * You'll get caught by the archer if you signal while they're awake.
- *
- * @param {boolean} archerIsAwake
- * @param {boolean} prisonerIsAwake
- *
- * @returns {boolean} Whether or not you can send a signal to the prisoner.
- */
-export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
-  throw new Error('Implement the canSignalPrisoner function');
-}
-
-/**
- * The final stage in the plan: freeing Annalyn's best friend.
- *
- * @param {boolean} knightIsAwake
- * @param {boolean} archerIsAwake
- * @param {boolean} prisonerIsAwake
- * @param {boolean} petDogIsPresent
- *
- * @returns {boolean} Whether or not you can free Annalyn's friend.
- */
-export function canFreePrisoner(
-  knightIsAwake,
-  archerIsAwake,
-  prisonerIsAwake,
-  petDogIsPresent
-) {
-  throw new Error('Implement the canFreePrisoner function');
-}
+ export function canExecuteFastAttack(knightIsAwake) {
+  return (!(knightIsAwake));
+  }
+  
+  
+  /**
+   * A useful spy captures information, which they can't do if everyone's asleep.
+   *
+   * @param {boolean} knightIsAwake
+   * @param {boolean} archerIsAwake
+   * @param {boolean} prisonerIsAwake
+   *
+   * @returns {boolean} Whether or not you can spy on someone.
+   */
+  export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+  let result; 
+    if (archerIsAwake || prisonerIsAwake || knightIsAwake){
+      result = true;
+    } else {
+      result = false;
+    } 
+    return result
+  }
+  /**
+   * You'll get caught by the archer if you signal while they're awake.
+   *
+   * @param {boolean} archerIsAwake
+   * @param {boolean} prisonerIsAwake
+   *
+   * @returns {boolean} Whether or not you can send a signal to the prisoner.
+   */
+  export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+  let result; 
+    if (!archerIsAwake && prisonerIsAwake){
+      result = true;
+    } else {
+      result = false;
+    } 
+    return result
+  }
+  
+  /**
+   * The final stage in the plan: freeing Annalyn's best friend.
+   *
+   * @param {boolean} knightIsAwake
+   * @param {boolean} archerIsAwake
+   * @param {boolean} prisonerIsAwake
+   * @param {boolean} petDogIsPresent
+   *
+   * @returns {boolean} Whether or not you can free Annalyn's friend.
+   */
+  export function canFreePrisoner(
+    knightIsAwake,
+    archerIsAwake,
+    prisonerIsAwake,
+    petDogIsPresent
+  ) {
+  let result; 
+    if (petDogIsPresent && !archerIsAwake){
+      result = true;
+    } else if (prisonerIsAwake && !archerIsAwake && !knightIsAwake) {
+      result = true;
+    } else if (!prisonerIsAwake) {
+      result = false;
+    } else {
+      result = false;
+    }
+    return result
+  }
--- exercises/concept/vehicle-purchase/vehicle-purchase.js
+++ exercises/concept/vehicle-purchase/vehicle-purchase.js
@@ -10,30 +10,51 @@
  * @param {string} kind
  * @returns {boolean} whether a license is required
  */
-export function needsLicense(kind) {
-  throw new Error('Please implement the needsLicense function');
-}
-
-/**
- * Helps choosing between two options by recommending the one that
- * comes first in dictionary order.
- *
- * @param {string} option1
- * @param {string} option2
- * @returns {string} a sentence of advice which option to choose
- */
-export function chooseVehicle(option1, option2) {
-  throw new Error('Please implement the chooseVehicle function');
-}
-
-/**
- * Calculates an estimate for the price of a used vehicle in the dealership
- * based on the original price and the age of the vehicle.
- *
- * @param {number} originalPrice
- * @param {number} age
- * @returns expected resell price in the dealership
- */
-export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Please implement the calculateResellPrice function');
-}
+ export function needsLicense(kind) {
+  let result;
+    if (kind === 'car')
+    result = true;  
+    else if (kind === 'truck')
+    result = true;
+    else
+    result = false;
+  return result
+  } 
+  
+  /**
+   * Helps choosing between two options by recommending the one that
+   * comes first in dictionary order.
+   *
+   * @param {string} option1
+   * @param {string} option2
+   * @returns {string} a sentence of advice which option to choose
+   */
+  export function chooseVehicle(option1, option2) {
+  let result;
+    option1.toLowerCase() 
+    option2.toLowerCase()
+    if (option1 < option2) 
+     result = option1
+    else
+     result = option2 
+  return result + " is clearly the better choice."
+  }
+  
+  /**
+   * Calculates an estimate for the price of a used vehicle in the dealership
+   * based on the original price and the age of the vehicle.
+   *
+   * @param {number} originalPrice
+   * @param {number} age
+   * @returns expected resell price in the dealership
+   */
+  export function calculateResellPrice(originalPrice, age) {
+  let result;
+  if (age < 3)
+  result = originalPrice * 80/100
+  else if (age > 10)
+  result = originalPrice * 50/100
+  else 
+  result = originalPrice * 70/100
+  return result
+  }
